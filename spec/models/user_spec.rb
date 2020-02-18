cde0ba48f683befcfb004dfca96f5b90f9173392
sdcro <sdcrouse91@gmail.com>
2020-02-17 21:12:14 -0800

    Fix syntax error in test and refactor tests to be more DRY

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,11 +15,6 @@ RSpec.describe User, type: :model do
     valid_user_attributes.except(:first_name, :last_name)
   end
 
-  # But these Users are invalid:
-  let(:missing_username) { valid_user_attributes.except(:username) }
-  let(:missing_email) { valid_user_attributes.except(:email) }
-  let(:missing_password) { valid_user_attributes.except(:password) }
-
   it "has a username, email, first_name, and last_name" do
     user = User.new(valid_user_attributes)
     expect(user.username).to eq("lorem.ipsum")
@@ -42,8 +37,15 @@ RSpec.describe User, type: :model do
     expect(User.new(missing_first_and_last_name)).to be_valid
   end
 
-  it "is invalid without a username" do
-    expect(User.new(missing_username)).to_not be_valid
+  it "is invalid without a username, email, or password" do
+    invalid_user = User.new
+
+    expect(invalid_user).to_not be_valid
+    expect(invalid_user.errors.full_messages).to include(
+      "Username can't be blank",
+      "Email can't be blank",
+      "Password can't be blank"
+    )
   end
 
   it "is invalid without a unique email or username" do
@@ -51,18 +53,10 @@ RSpec.describe User, type: :model do
     same_user = User.create(valid_user_attributes)
 
     expect(same_user).to_not be_valid
-    expect(same_user).errors.full_messages.to include(
+    expect(same_user.errors.full_messages).to include(
       "Username has already been taken",
       "Email has already been taken"
     )
-  end
-
-  it "is invalid without an email" do
-    expect(User.new(missing_email)).to_not be_valid
-  end
-
-  it "is invalid without a password" do
-    expect(User.new(missing_password)).to_not be_valid
   end
 
 end
