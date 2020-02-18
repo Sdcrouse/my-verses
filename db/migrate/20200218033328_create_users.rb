f7487e81ca77a4938dc8472401ca86f1e62efebc
sdcro <sdcrouse91@gmail.com>
2020-02-17 19:40:14 -0800

    Use Rails resource generator to create a User model, migration, controller, tests, etc.

---
+++ db/migrate/20200218033328_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      
+      t.timestamps
+    end
+  end
+end
