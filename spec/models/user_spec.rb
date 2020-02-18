ce53f288771d75b73cac073a412d7c65f33db6fc
sdcro <sdcrouse91@gmail.com>
2020-02-17 20:28:56 -0800

    Add additional (passing) User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,18 @@ RSpec.describe User, type: :model do
     }
   end
 
+  let(:missing_first_and_last_name) do
+    valid_user_attributes.except(:first_name, :last_name)
+  end
+
+  it "has a username, email, first_name, and last_name" do
+    user = User.new(valid_user_attributes)
+    expect(user.username).to eq("lorem.ipsum")
+    expect(user.email).to eq("dolorsitamet@gmail.com")
+    expect(user.first_name).to eq("Lorem")
+    expect(user.last_name).to eq("Ipsum")
+  end
+
   it "is valid" do
     expect(User.new(valid_user_attributes)).to be_valid
   end
@@ -19,6 +31,10 @@ RSpec.describe User, type: :model do
     user = User.create(valid_user_attributes)
     expect(user.id).to_not be_nil
     expect(User.last).to eq(user)
+  end
+
+  it "is valid without a first name and last name" do
+    expect(User.new(missing_first_and_last_name)).to be_valid
   end
 
 end
