5a10f867d3c5fd23ad5c14aa24f26a496169984c
sdcro <sdcrouse91@gmail.com>
2020-02-19 00:53:18 -0800

    Overwrite the VerseReference model's #book= writer method
    As noted in the comments, I have chosen not to add a validation for a titleized book.
    Instead, I overwrote the #book= writer method to titleize the book for any user input.
    This passes the corresponding Rspec test that I wrote in the last commit.

--- app/models/verse_reference.rb
+++ app/models/verse_reference.rb
@@ -1,4 +1,13 @@
 class VerseReference < ApplicationRecord
   validates :book, presence: true
   validates :chapter, presence: true
+
+  def book=(book)
+    # Instead of validating for a titleized book, 
+    # I will convert it to one here.
+    # However, the uniqueness validation should be called AFTER this method is run.
+    
+    # write_attribute(:book, book.downcase.titleize) # This works.
+    super(book.downcase.titleize) # But this is the Rails way to do it.
+  end
 end
