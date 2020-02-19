3c580f972813491edaaa3c3b4f6a8f6da958e047
sdcro <sdcrouse91@gmail.com>
2020-02-18 17:51:53 -0800

    Run migration to create the verse_references table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_033328) do
+ActiveRecord::Schema.define(version: 2020_02_18_082536) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_02_18_033328) do
     t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "verse_references", force: :cascade do |t|
+    t.string "book"
+    t.string "chapter"
+    t.string "verse_start"
+    t.string "verse_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
