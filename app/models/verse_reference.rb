aa61b106d7a2861001246cbe6a010137ad7066ef
sdcro <sdcrouse91@gmail.com>
2020-02-19 17:04:03 -0800

    Remove inaccurate comment from VerseReference method
    Contrary to the removed comment, I don't need uniqueness validations on the VerseReference model.
    Instead, I will call #find_or_create_by, then associate that VerseReference to a MyVerse.

--- app/models/verse_reference.rb
+++ app/models/verse_reference.rb
@@ -8,7 +8,6 @@ class VerseReference < ApplicationRecord
   def book=(book)
     # Instead of validating for a titleized book, 
     # I will convert it to one here.
-    # However, the uniqueness validation should be called AFTER this method is run.
     
     # write_attribute(:book, book.downcase.titleize) # This works.
     super(book.downcase.titleize) # But this is the Rails way to do it.
