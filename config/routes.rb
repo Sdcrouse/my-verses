eb942aaafa5ad15ac91dacd1b909e9caf095e0e0
sdcro <sdcrouse91@gmail.com>
2020-02-20 15:07:35 -0800

    Generate homepage controller, route, view, SCSS file, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :verse_references
   resources :users
+  # Next steps: Homepage, User sign up, login, and logout
+
+  root 'static#home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
