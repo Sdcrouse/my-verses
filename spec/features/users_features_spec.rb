ad48bb646adeaec9dc87192c3a267feb1cefdea6
sdcro <sdcrouse91@gmail.com>
2020-02-20 19:23:19 -0800

    Change test to look for different text on user signup page

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe "Feature Test: User Signup", type: :feature do
 
     it "has fields for username, email, first name, last name, and password" do
       expect(page).to have_content("Username")
-      expect(page).to have_content("Email")
-      expect(page).to have_content("First Name")
-      expect(page).to have_content("Last Name")
+      expect(page).to have_content("E-mail")
+      expect(page).to have_content("First name")
+      expect(page).to have_content("Last name")
       expect(page).to have_content("Password")
     end
   end
