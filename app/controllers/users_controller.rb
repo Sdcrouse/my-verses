102be9e5661c83a8f9d5dc58c43fccc937c89878
sdcro <sdcrouse91@gmail.com>
2020-02-22 19:26:27 -0800

    Add Pry gem to project, and test it in the UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  require 'pry'
+
   def new
     @user = User.new
+  end
+
+  # Next step: The #create action.
+  def create
+    binding.pry
   end
 end
