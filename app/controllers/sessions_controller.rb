ee89ec18a16430c58d87a441dba5cec777029ee7
sdcro <sdcrouse91@gmail.com>
2020-02-25 15:46:45 -0800

    Write #create action in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user.try(:authenticate, params[:user][:password])
+      session[:user_id] = @user.id
+      binding.pry
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def destroy
