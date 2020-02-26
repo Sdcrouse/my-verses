3bdd3e37e2bfe11bba1f45161f8c18a239efc2f4
sdcro <sdcrouse91@gmail.com>
2020-02-25 16:55:46 -0800

    Add tests for User model's #full_name method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,9 +11,19 @@ RSpec.describe User, type: :model do
     }
   end
 
-  let(:missing_first_and_last_name) do # And this User is valid.
+  #----- Other valid Users ----------------
+  let(:missing_first_name) do
+    valid_user_attributes.except(:first_name)
+  end
+  
+  let(:missing_last_name) do
+    valid_user_attributes.except(:last_name)
+  end
+
+  let(:missing_first_and_last_name) do
     valid_user_attributes.except(:first_name, :last_name)
   end
+  #----- End of other valid Users ----------
 
   it "has a username, email, first_name, and last_name" do
     user = User.new(valid_user_attributes)
@@ -57,6 +67,24 @@ RSpec.describe User, type: :model do
       "Username has already been taken",
       "Email has already been taken"
     )
+  end
+
+  it "has a full_name if first and last names are present" do
+    user = User.new(valid_user_attributes)
+    expect(user.full_name).to eq("Lorem Ipsum")
+  end
+
+  it "does not have a full_name if first_name or last_name is absent" do
+    users_with_missing_names = [
+      missing_first_name, 
+      missing_last_name, 
+      missing_first_and_last_name
+    ]
+
+    users_with_missing_names.each do |user_attributes|
+      user = User.new(user_attributes)
+      expect(user.full_name).to be_nil
+    end
   end
 
 end
