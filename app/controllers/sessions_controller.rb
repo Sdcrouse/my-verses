afa005f0df7997ffa24524b6f5c8352111d172d2
sdcro <sdcrouse91@gmail.com>
2020-02-27 17:14:54 -0800

    Add and style flash message for successfully logging in, and fix bug
    The bug was introduced by the previous commit:
    Whenever a User failed to log in, and then logged in successfully,
    they would be correctly redirected to their profile,
    but the previous failed-login error message would be displayed.
    
    This commit fixes that bug by adding a success message, but I should watch for similar bugs.
    I may need to re-write the partial, or else call it only from the view files that display flash messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
 
     if @user.try(:authenticate, params[:password])
       session[:user_id] = @user.id
+      flash[:success] = "You are logged in!"
       redirect_to user_path(@user)
     else
       flash[:error] = "Invalid username and/or password."
