726935277ddcda1ea1404c3fb9796700fac46bd6
sdcro <sdcrouse91@gmail.com>
2020-03-02 21:37:06 -0800

    Refactor SessionsController with a helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,7 @@ class SessionsController < ApplicationController
     # Here, I am assuming that no two users have the same email, and that the same user has only one account.
 
     if @user.try(:authenticate, params[:password])
-      session[:user_id] = @user.id
-      flash[:success] = "You are logged in!"
-      redirect_to user_path(@user)
+      login_user(@user)
     else
       flash[:error] = "Invalid email and/or password."
       render :new
@@ -34,9 +32,7 @@ class SessionsController < ApplicationController
   def fb_login
     user = User.from_fb_omniauth(auth_info)
     user.save
-    session[:user_id] = user.id
-    flash[:success] = "You are logged in!"
-    redirect_to user_path(user)
+    login_user(user)
 
     # I could add an error message here, but that would be a seldom-encountered edge case.
   end
@@ -45,5 +41,11 @@ class SessionsController < ApplicationController
 
     def auth_info
       request.env['omniauth.auth']['info']
+    end
+
+    def login_user(user)
+      session[:user_id] = user.id
+      flash[:success] = "You are logged in!"
+      redirect_to user_path(user)
     end
 end
