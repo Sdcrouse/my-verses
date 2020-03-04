0b2446d19a9d6eda292aaeec1d3ab57fa79b21a5
sdcro <sdcrouse91@gmail.com>
2020-03-03 17:25:33 -0800

    Add helper method for displaying a VerseReference

--- app/helpers/verse_references_helper.rb
+++ app/helpers/verse_references_helper.rb
@@ -1,2 +1,20 @@
 module VerseReferencesHelper
+  def display_reference(ref)
+    # Example return values:
+      # Psalm 23
+      # John 3:16
+      # Proverbs 3:5-6
+
+    reference_notation = "#{ref.book} #{ref.chapter}"
+
+    if ref.verse_start
+      reference_notation += ":#{ref.verse_start}"
+
+      if ref.verse_end
+        reference_notation += "-#{verse_end}"
+      end
+    end
+
+    reference_notation
+  end
 end
