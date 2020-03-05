0a4844bfdbee2aabf53c94a84ce07f61037deb73
sdcro <sdcrouse91@gmail.com>
2020-03-04 19:30:52 -0800

    Add a test for a valid MyVerse

--- spec/models/my_verse_spec.rb
+++ spec/models/my_verse_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe MyVerse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Valid MyVerses:
+  let(:valid_myverse1) do
+    {
+      version: "NIV",
+      verse_text: "In the beginning God created the heavens and the earth.",
+      reason_liked: "This is the verse that starts it all!",
+      verse_reference_id: 1,
+      user_id: 1
+    }
+  end
+  # End of valid MyVerses
+
+  it "has a version, verse_text, reason_liked, verse_reference_id, and user_id, and is valid" do
+    my_verse = MyVerse.new(valid_myverse1)
+
+    expect(my_verse.version).to eq("NIV")
+    expect(my_verse.verse_text).to eq("In the beginning God created the heavens and the earth.")
+    expect(my_verse.reason_liked).to eq("This is the verse that starts it all!")
+    expect(my_verse.verse_reference_id).to eq(1)
+    expect(my_verse.user_id).to eq(1)
+
+    expect(my_verse).to be_valid
+  end
+
 end
