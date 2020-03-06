b52c16ff835d2c11b8b24f12ee0cfdbf3425e303
sdcro <sdcrouse91@gmail.com>
2020-03-05 16:39:31 -0800

    Refactor the MyVerse tests to be more DRY and obey the Single Responsibility Principle

--- spec/models/my_verse_spec.rb
+++ spec/models/my_verse_spec.rb
@@ -14,23 +14,20 @@ RSpec.describe MyVerse, type: :model do
       version: "NIV",
       verse_text: "In the beginning God created the heavens and the earth.",
       reason_liked: "This is the verse that starts it all!",
-      verse_reference_id: first_verse_reference.id,
-      user_id: moses.id
+      user: moses,
+      verse_reference: first_verse_reference       
     }
   end
 
-  let(:no_user_or_reference_ids) { valid_myverse.except(:verse_reference_id, :user_id) }
-
-  it "has a version, verse_text, reason_liked, verse_reference_id, and user_id, and is valid" do
+  it "is valid with a version, verse_text, reason_liked, verse_reference, and user" do
     my_verse = MyVerse.new(valid_myverse)
-
+    
+    expect(my_verse).to be_valid
     expect(my_verse.version).to eq("NIV")
     expect(my_verse.verse_text).to eq("In the beginning God created the heavens and the earth.")
     expect(my_verse.reason_liked).to eq("This is the verse that starts it all!")
-    expect(my_verse.verse_reference_id).to eq(1)
-    expect(my_verse.user_id).to eq(1)
-
-    expect(my_verse).to be_valid
+    expect(my_verse.verse_reference).to eq(first_verse_reference)
+    expect(my_verse.user).to eq(moses)
   end
 
   it "is valid without a reason_liked" do
@@ -50,16 +47,4 @@ RSpec.describe MyVerse, type: :model do
     )
   end
 
-  it "belongs to a User and a VerseReference" do
-    moses = User.create(username: "moses", email: "lawgiver@israel.com", password: "tencommandments")
-    ref = VerseReference.create(book: "Genesis", chapter: "1", verse_start: "1")
-
-    myverse_with_user_and_reference = no_user_or_reference_ids.merge(user: moses, verse_reference: ref)
-    moses_myverse = MyVerse.create(myverse_with_user_and_reference)
-
-    expect(moses_myverse).to be_valid
-    expect(moses_myverse.user).to eq(moses)
-    expect(moses_myverse.verse_reference).to eq(ref)
-  end
-  
 end
