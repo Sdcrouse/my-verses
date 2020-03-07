686164fe084fd254809933aa180c706725059aaa
sdcro <sdcrouse91@gmail.com>
2020-03-06 16:17:32 -0800

    Add Users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users:
+johnny = User.create(
+  username: "johnny.appleseed",
+  email: "jchapman@penn.edu",
+  first_name: "John",
+  last_name: "Chapman",
+  password: "orchards"
+)
+
+paul = User.create(
+  username: "pauloftarsus",
+  email: "p.apostle@israel.com",
+  password: "righteousnessByFaith"
+)
+
+peter = User.create(
+  username: "simonpeter",
+  email: "s.peter@gmail.com",
+  first_name: "Simon",
+  last_name: "Peter"
+  password: "fishersofmen"
+)
+
+newguy = User.create(username: "NewConvert", email: "loremipsum@dolorsit.amet", password: "pw")
+
