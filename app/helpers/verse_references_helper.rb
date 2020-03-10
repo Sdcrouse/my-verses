2abe020a4484df412d48de13350b990ae7f27207
sdcro <sdcrouse91@gmail.com>
2020-03-10 16:23:01 -0700

    Add note and fix VerseReference display issue
    Originally, when the User created a VerseReference with a verse_start, but no verse_end, it would display like this:
    John 1:1-, Proverbs 3:5-, etc. This commit solves that problem; now they look like John 1:1 and Proverbs 3:5.

--- app/helpers/verse_references_helper.rb
+++ app/helpers/verse_references_helper.rb
@@ -7,10 +7,10 @@ module VerseReferencesHelper
 
     reference_notation = "#{ref.book} #{ref.chapter}"
 
-    if ref.verse_start
+    if ref.verse_start # Not (yet) sure why I don't have to call #present? here.
       reference_notation += ":#{ref.verse_start}"
 
-      if ref.verse_end
+      if ref.verse_end.present?
         reference_notation += "-#{ref.verse_end}"
       end
     end
