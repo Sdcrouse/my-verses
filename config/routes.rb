513f32d98cb433ecc57b0c67fd05eebb90ba3817
sdcro <sdcrouse91@gmail.com>
2020-03-09 18:55:35 -0700

    Move #create route from MyVerse to VerseReference

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/my_verses/new", to: "verse_references#new", as: "new_my_verse"
   # Note: I'm setting my routes up this way so that I can use #accepts_nested_attributes_for on the VerseReference model.
-  resources :my_verses, except: [:new]
+  resources :my_verses, except: [:new, :create]
   
   get '/auth/facebook/callback' => 'sessions#fb_login'
 
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :verse_references, only: :index
+  resources :verse_references, only: [:index, :create]
 
   get '/signup', to: 'users#new'
   resources :users, except: [:index, :new]
