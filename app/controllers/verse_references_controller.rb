55ef1d7da7eccb9b37d5299b7cc6af6ec2440b04
sdcro <sdcrouse91@gmail.com>
2020-03-10 16:16:07 -0700

    Write a basic #create action for the VerseReferencesController

--- app/controllers/verse_references_controller.rb
+++ app/controllers/verse_references_controller.rb
@@ -13,7 +13,19 @@ class VerseReferencesController < ApplicationController
   end
 
   def create
-    binding.pry
+    @verse_reference = VerseReference.new(verse_reference_params)
+    myverse = @verse_reference.my_verses.last
+    @verse_reference.save
+    redirect_to my_verse_path(myverse)
+    # binding.pry
     # Note: The hidden_field can be hacked, so make sure that the user_id is the same as current_user.id.
   end
+
+  private
+    def verse_reference_params
+      params.require(:verse_reference).permit(
+        :book, :chapter, :verse_start, :verse_end, 
+        my_verses_attributes: [:user_id, :version, :verse_text, :reason_liked]
+      )
+    end
 end
