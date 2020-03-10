da7da3c24f316b3f197c9e6399e09f1c9d8de9fd
sdcro <sdcrouse91@gmail.com>
2020-03-09 18:47:34 -0700

    Refactor VerseReferencesController with DRY code

--- app/controllers/verse_references_controller.rb
+++ app/controllers/verse_references_controller.rb
@@ -1,11 +1,11 @@
 class VerseReferencesController < ApplicationController
+  before_action :user_must_be_logged_in
+
   def index
-    user_must_be_logged_in
     @verse_references = VerseReference.all
   end
 
   def new
-    user_must_be_logged_in
     @verse_reference = VerseReference.new
   end
 end
