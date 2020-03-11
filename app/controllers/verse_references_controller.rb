d4338584a19e97ab0a7acb799cb28bcbb863d60b
sdcro <sdcrouse91@gmail.com>
2020-03-10 17:13:21 -0700

    Prevent Users from creating MyVerses for other Users

--- app/controllers/verse_references_controller.rb
+++ app/controllers/verse_references_controller.rb
@@ -15,10 +15,12 @@ class VerseReferencesController < ApplicationController
   def create
     @verse_reference = VerseReference.new(verse_reference_params)
     myverse = @verse_reference.my_verses.last
+  
+    check_user_id_of(myverse) and return
+
     @verse_reference.save
     redirect_to my_verse_path(myverse)
     # binding.pry
-    # Note: The hidden_field can be hacked, so make sure that the user_id is the same as current_user.id.
   end
 
   private
@@ -27,5 +29,12 @@ class VerseReferencesController < ApplicationController
         :book, :chapter, :verse_start, :verse_end, 
         my_verses_attributes: [:user_id, :version, :verse_text, :reason_liked]
       )
+    end
+
+    def check_user_id_of(myverse)
+      if myverse.user_id != current_user.id
+        flash[:error] = "Impressive hacking, but you're not allowed to write a MyVerse for somebody else."
+        render :new
+      end
     end
 end
