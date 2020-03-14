a872d9c694d335a41d006b50645b71a70ad599bc
sdcro <sdcrouse91@gmail.com>
2020-03-14 00:35:49 -0700

    Remove unnecessary actions and condense the code in the VerseReferencesController
    This commit completes what I started many commits ago:
    Moving the responsibility for creating a new MyVerse and VerseReference from the VerseReference model, controller, etc,.
    to the MyVerse model, controller, etc.
    The actions that I deleted are no longer needed. For good measure, I made the remaining code more readable by condensing it.

--- app/controllers/verse_references_controller.rb
+++ app/controllers/verse_references_controller.rb
@@ -1,40 +1,6 @@
-require 'pry'
-
 class VerseReferencesController < ApplicationController
-  before_action :user_must_be_logged_in
-
   def index
+    user_must_be_logged_in
     @verse_references = VerseReference.all
   end
-
-  def create
-    @verse_reference = VerseReference.new(verse_reference_params)
-    myverse = @verse_reference.my_verses.last
-  
-    check_user_id_of(myverse) and return
-
-    if @verse_reference.save
-      flash[:success] = "Success! Here is your new MyVerse, #{current_user.username}:"
-      redirect_to my_verse_path(myverse)
-    else
-      # Note: Since the form creates a VerseReference AND a MyVerse, the myverse error messages look a bit weird.
-      # I can try to adjust that later as a stretch goal.
-      render :new
-    end
-  end
-
-  private
-    def verse_reference_params
-      params.require(:verse_reference).permit(
-        :book, :chapter, :verse_start, :verse_end, 
-        my_verses_attributes: [:user_id, :version, :verse_text, :reason_liked]
-      )
-    end
-
-    def check_user_id_of(myverse)
-      if myverse.user_id != current_user.id
-        flash[:error] = "Impressive hacking, but you're not allowed to write a MyVerse for somebody else."
-        render :new
-      end
-    end
 end
