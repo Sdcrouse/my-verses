8d0ca2f6086ad6d55019e267cf2961b947b50dfe
sdcro <sdcrouse91@gmail.com>
2020-03-13 21:16:24 -0700

    Create new (and unique) MyVerses and VerseReferences
    This took a while to figure out.
    I found a way to create a new MyVerse and associate it with a new or already-existing VerseReference.
    Time will tell if I implemented this correctly.

--- app/controllers/my_verses_controller.rb
+++ app/controllers/my_verses_controller.rb
@@ -15,11 +15,31 @@ class MyVersesController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    @my_verse = MyVerse.new(my_verse_params)
+    @my_verse.verse_reference = VerseReference.find_or_initialize_by(verse_reference_params)
+    # The line above probably needs to be refactored into a MyVerse method, but I don't yet know how.
+    
+    if @my_verse.save
+      flash[:success] = "Success! Here is your new MyVerse, #{current_user.username}:"
+      redirect_to my_verse_path(@my_verse)
+    end
   end
 
   def edit
     @my_verse = MyVerse.find(params[:id])
     @verse_reference = @my_verse.verse_reference
   end
+
+  private
+    def my_verse_params
+      params.require(:my_verse).permit(
+        :user_id, :version, :verse_text, :reason_liked
+      )
+    end
+
+    def verse_reference_params
+      params[:my_verse].require(:verse_reference).permit(
+        :book, :chapter, :verse_start, :verse_end
+      )
+    end
 end
