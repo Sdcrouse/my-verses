d7225b5b8cc069fe0c2321a8a9740778102e3b38
sdcro <sdcrouse91@gmail.com>
2020-03-18 16:44:43 -0700

    Have view files use a model method instead of a view helper method, and delete the view helper method
    Since a verse_reference is the same (in my eyes) as a citation,
    it makes more sense to have my views call VerseReference#citation_format
    instead of VerseReferencesHelper#display_reference.
    
    I am deleting #display_reference, as it is no longer needed.
    Also, I have to put all of these changes in one commit, or else my program will break here.

--- app/helpers/verse_references_helper.rb
+++ app/helpers/verse_references_helper.rb
@@ -1,20 +1,2 @@
 module VerseReferencesHelper
-  def display_reference(ref)
-    # Example return values:
-      # Psalm 23
-      # John 3:16
-      # Proverbs 3:5-6
-
-    reference_notation = "#{ref.book} #{ref.chapter}"
-
-    if ref.verse_start.present?
-      reference_notation += ":#{ref.verse_start}"
-
-      if ref.verse_end.present?
-        reference_notation += "-#{ref.verse_end}"
-      end
-    end
-
-    reference_notation
-  end
 end
