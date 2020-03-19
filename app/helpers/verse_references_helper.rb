384c0aee6675646bf4841d82bc34bd12f8bf5cfc
sdcro <sdcrouse91@gmail.com>
2020-03-19 16:37:43 -0700

    Refactor index files with VerseReferencesHelper method

--- app/helpers/verse_references_helper.rb
+++ app/helpers/verse_references_helper.rb
@@ -1,2 +1,7 @@
 module VerseReferencesHelper
+  def link_to_new_myverse_with_reference(ref)
+    if !current_user.has_verse_reference?(ref)
+      link_to "Create a MyVerse with this Verse Reference", new_verse_reference_my_verse_path(ref)
+    end
+  end
 end
