39ff2025da34dea110c667631f7fa6b19dbc55b1
sdcro <sdcrouse91@gmail.com>
2020-03-28 19:00:58 -0700

    Change links to buttons on multiple pages

--- app/helpers/verse_references_helper.rb
+++ app/helpers/verse_references_helper.rb
@@ -1,7 +1,9 @@
 module VerseReferencesHelper
-  def link_to_new_myverse_with_reference(ref)
+  def button_to_new_myverse_with_reference(ref)
     if !current_user.has_verse_reference?(ref)
-      link_to "Create a MyVerse with this Verse Reference", new_verse_reference_my_verse_path(ref)
+      button_to("Create a MyVerse with this Verse Reference", 
+        new_verse_reference_my_verse_path(ref), method: :get
+      )
     end
   end
 end
