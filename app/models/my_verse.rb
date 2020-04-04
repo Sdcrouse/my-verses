645588ce9aa7c900a1c2af942a1969e29a7b632a
sdcro <sdcrouse91@gmail.com>
2020-04-03 17:41:36 -0700

    Organize MyVerse model into sections

--- app/models/my_verse.rb
+++ app/models/my_verse.rb
@@ -1,24 +1,14 @@
 class MyVerse < ApplicationRecord
+  # -------- Associations and validations ----------------
   belongs_to :user
   belongs_to :verse_reference
   accepts_nested_attributes_for :verse_reference
 
-  default_scope { order_by_reference_version_and_username }
-
   validates :version, presence: true
   validates :verse_text, presence: true
 
-  def user_name
-    self.user.username
-  end
-
-  def belongs_to_user?(user)
-    self.user == user
-  end
-
-  def reference_citation
-    self.verse_reference.citation_format
-  end
+  # --------- Scopes ---------------
+  default_scope { order_by_reference_version_and_username }
 
   def self.order_by_reference_version_and_username
     # Due to the complexity of this scope, I am choosing not to use the #scope syntactic sugar here.
@@ -34,5 +24,18 @@ class MyVerse < ApplicationRecord
     # I am using a class method here because, according to the documentation at https://guides.rubyonrails.org/active_record_querying.html#passing-in-arguments,
     # "using a class method is the preferred way to accept arguments for scopes".
     joins(:verse_reference).merge( VerseReference.within_book(book_title) )
+  end
+
+  # ---------- Helper methods ---------------
+  def user_name
+    self.user.username
+  end
+
+  def belongs_to_user?(user)
+    self.user == user
+  end
+
+  def reference_citation
+    self.verse_reference.citation_format
   end
 end
